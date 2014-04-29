ba44f03d0bde41aebf7e88c033e5fd737bbbd7b8
Urs Langenegger <urs.langenegger@psi.ch>
2014-04-29 09:10:07 +0200

    ...

--- search/all_3.js
+++ search/all_3.js
@@ -4,5 +4,6 @@ var searchData=
   ['clearhistlist',['clearHistList',['../classPixTest.html#a380ffe5d8b789c9765cf09428fab3077',1,'PixTest']]],
   ['clearselectedpixels',['clearSelectedPixels',['../classPixTest.html#a6b9238aa60242df30cb904983e49c601',1,'PixTest']]],
   ['configenableset',['configEnableSet',['../classpxar_1_1configEnableSet.html',1,'pxar']]],
-  ['configmaskset',['configMaskSet',['../classpxar_1_1configMaskSet.html',1,'pxar']]]
+  ['configmaskset',['configMaskSet',['../classpxar_1_1configMaskSet.html',1,'pxar']]],
+  ['configparameters',['ConfigParameters',['../classConfigParameters.html',1,'']]]
 ];
